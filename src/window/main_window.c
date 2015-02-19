94bfcd576fbd156ac210d4ef97dfd0ebe0172e3e
mvaude <mvaude@student.42.fr>
2015-02-19 01:39:16 +0100

    FIX info_layer

--- src/window/main_window.c
+++ src/window/main_window.c
@@ -8,7 +8,7 @@ void		main_window_load(Window *window)
 	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
-	s_info_layer = text_layer_create(GRect(0, 5, 144, 163));
+	s_info_layer = text_layer_create(GRect(0, 5, 144, 168));
 	text_layer_set_background_color(s_info_layer, GColorClear);
 	text_layer_set_text_color(s_info_layer, GColorBlack);
 	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
