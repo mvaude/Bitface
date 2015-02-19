f0b3df7245fa296eb821d3f94639dfdd0236dc0d
mvaude <mvaude@student.42.fr>
2015-02-19 04:03:42 +0100

    FIX tick_handler.c

--- src/time/tick_handler.c
+++ src/time/tick_handler.c
@@ -3,4 +3,14 @@
 void		tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
 	update_time();
+
+	// Get infos update every 10 minutes
+	if (!(tick_time->tm_min % 10))
+	{
+		DictionaryIterator	*iter;
+		app_message_outbox_begin(&iter);
+
+		dict_write_uint8(iter, 0, 0);
+		app_message_outbox_send();
+	}
 }
